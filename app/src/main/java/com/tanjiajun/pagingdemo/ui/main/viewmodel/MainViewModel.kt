2ad76bc3e0edf18f1dca8da09a00c7c9a26c3573
谭嘉俊 <1120571286@qq.com>
2021-08-08 00:50:34 +0800

    新建项目。

--- app/src/main/java/com/tanjiajun/pagingdemo/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tanjiajun/pagingdemo/ui/main/viewmodel/MainViewModel.kt
@@ -9,10 +9,7 @@ import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.tanjiajun.pagingdemo.data.model.main.RepositoryData
 import com.tanjiajun.pagingdemo.data.repository.GitHubRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * Created by TanJiaJun on 7/26/21.
@@ -29,40 +26,11 @@ class MainViewModel(
     private val _isShowErrorView = MutableLiveData<Boolean>()
     val isShowErrorView: LiveData<Boolean> = _isShowErrorView
 
-    // 是否显示仓库页面
-    private val _isShowRepositoryView = MutableLiveData<Boolean>()
-    val isShowRepositoryView: LiveData<Boolean> = _isShowRepositoryView
-
-    private val _repositories = MutableLiveData<List<RepositoryData>>()
-    val repositories: LiveData<List<RepositoryData>> = _repositories
-
     fun getRepositories(languageName: String): Flow<PagingData<RepositoryData>> =
         repository.getRepositories(
             languageName = languageName,
-            pageSize = 10
+            pageSize = 20
         ).cachedIn(viewModelScope)
-
-//    fun getRepositories(languageName: String) {
-//        viewModelScope.launch {
-//            _isShowLoadingView.value = true
-//            try {
-//                withContext(Dispatchers.IO) {
-//                    val repositories: List<RepositoryData> =
-//                        repository.getRepositories(languageName)
-//                    withContext(Dispatchers.Main) {
-//                        if (repositories.isNotEmpty()) {
-//                            _repositories.value = repositories
-//                            _isShowRepositoryView.value = true
-//                        }
-//                    }
-//                }
-//            } catch (throwable: Throwable) {
-//                _isShowErrorView.value = true
-//            } finally {
-//                _isShowLoadingView.value = false
-//            }
-//        }
-//    }
 
     interface Handlers {
 
